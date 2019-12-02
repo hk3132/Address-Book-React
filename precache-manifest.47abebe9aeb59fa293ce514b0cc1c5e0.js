93559e9cfdd9c6405ed1ff68be9fadc8c68b82ec
Code Nation <codenation@Codes-MacBook-Pro.local>
2019-12-02 11:00:41 +0000

    Updates

---
+++ precache-manifest.47abebe9aeb59fa293ce514b0cc1c5e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bd89403ebd0bafec35149916761ac25",
+    "url": "/Address-Book-React/index.html"
+  },
+  {
+    "revision": "0c6dfaf05d5525064436",
+    "url": "/Address-Book-React/static/css/main.926045af.chunk.css"
+  },
+  {
+    "revision": "a7c31df75ba63b55ebcf",
+    "url": "/Address-Book-React/static/js/2.a6ea3f5c.chunk.js"
+  },
+  {
+    "revision": "0c6dfaf05d5525064436",
+    "url": "/Address-Book-React/static/js/main.fb003212.chunk.js"
+  },
+  {
+    "revision": "9e5afb04e383269b76ec",
+    "url": "/Address-Book-React/static/js/runtime-main.7f7823d7.js"
+  }
+]);
